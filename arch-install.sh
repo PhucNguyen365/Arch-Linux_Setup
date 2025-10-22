11821bd8867459216e7bd8818472d8fafc9934e9
PhucNguyen365 <phuctechnical365@gmail.com>
2025-10-22 01:55:06 -0700

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ===========================================================
 #  Automated Arch Linux Installation Script (UEFI)
-#  Author: Technical (Clean & Final Version)
+#  Author: Technical
 # ===========================================================
 
 set -euo pipefail
